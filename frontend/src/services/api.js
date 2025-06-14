bf9ca6c0f4c6ebc780f4ffdc14abe7d546e5f381
Mariano <marianoemgonzalez1@gmail.com>
2025-06-14 18:03:44 -0300

    Agrego el archivo api.js y cambio authContext para redirigir las peticiones

---
+++ frontend/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: { 'x-auth-token': localStorage.getItem('token') || '' },
+});
+
+export default api;
